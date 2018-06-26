9c075975bf45c2f64af84ab3c2f68edac53a36c7
Gabriel Williams <gwgabew@gmail.com>
2018-06-26 13:07:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -36,9 +36,6 @@ function sayHiToGrandma(string){
       return("YES INDEED!")
   }
   else if(string==="I love you, Grandma."){
-      return("I love you, too!")
+      return("I love you, too.")
   }
   
-  
-  return ("I can't hear you!")
-}
